524b4cb36bebf0cc9be19ed15ca1724a8a726eb3
inciarmors <mors.jpg@gmai.com>
2025-08-24 13:37:45 +0200

    chore: clean repository according to updated .gitignore

--- dist/checklist.d.ts
+++
@@ -1,7 +0,0 @@
-import { ChecklistRule } from './config';
-/**
- * Genera una checklist markdown a partire dalle regole matchate.
- * @param rules Array di regole matchate
- * @returns Stringa markdown pronta da inserire in un commento PR
- */
-export declare function generateChecklistMarkdown(rules: ChecklistRule[]): string;
